c2d5f8472b4a702144950ffb63061ff63d3d968c
sandeepsachdev <sandeepsachdev17@gmail.com>
2026-04-26 08:17:13 +1000

    add smh and fix up nav for mobile

--- src/main/java/com/scrollingnews/service/NewsAggregatorService.java
+++ src/main/java/com/scrollingnews/service/NewsAggregatorService.java
@@ -47,7 +47,8 @@ public class NewsAggregatorService {
             new NewsSource("Sky News",        "https://feeds.skynews.com/feeds/rss/home.xml"),
             new NewsSource("Fox News",        "https://moxie.foxnews.com/google-publisher/latest.xml"),
             new NewsSource("Engadget",        "https://www.engadget.com/rss.xml"),
-            new NewsSource("ABC News",        "https://abcnews.go.com/abcnews/topstories")
+            new NewsSource("ABC News",        "https://abcnews.go.com/abcnews/topstories"),
+            new NewsSource("SMH News",        "https://www.smh.com.au/rss/feed.xml")
     );
 
     private record SequencedArticle(long seq, NewsArticle article) {}
